464308fdb72408a8550cad6a1182b8bebd930d18
QuincyO <quincyorta@gmail.com>
2023-06-28 02:30:37 -0400

    Implemented Tille

---
+++ game/src/TileMap.h
@@ -0,0 +1,89 @@
+#pragma once
+#include <vector>
+#include "rlImGui.h"
+#include "TileCoord.h"
+#include "raylib.h"
+#include "Sprite.h"
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+#define MAP_WIDTH (SCREEN_WIDTH/64)
+#define MAP_HEIGHT (SCREEN_HEIGHT/64)
+
+using namespace std;
+enum class Tile 
+{
+	Floor = 0, //Floor tiles can be walked on
+	Wall, //Wall tiles cannot be walked on
+	Count
+
+};
+
+
+
+
+class Tilemap
+{
+
+public:
+	Tilemap(const char* filepath) : player(filepath) {}
+	Tilemap() {}
+private:
+
+
+	Tile tiles[MAP_WIDTH][MAP_HEIGHT]; //Standard array cannot be chaged at runtime
+
+	const Vector2 NORTH = { 0,-1 };
+	const Vector2 SOUTH = { 0,1 };
+	const Vector2 EAST = { 1,0 };
+	const Vector2 WEST = { -1,0 };
+
+	TileCoord* tilePosition;
+	int width = 0;
+	int height = 0;
+vector<vector<Tile>> v_Tiles; //Dynamic Array that can be changed during Runtime
+
+public:
+	Sprite player;
+
+	int tileSizeX = 64;
+	int tileSizeY = 64;
+
+public:
+	int GetWidth()const { return width; }
+	int GetHeight() const { return height; }
+
+	int GetGridWidth() {return MAP_WIDTH; } //Get Number of columns in the grid
+	int GetGridHeight() { return MAP_HEIGHT; } //Get Number of rows in the grid
+	Tile GetTile(int x, int y); //Get the tile at the specified coordinate in the grid
+	Tile GetTile(Vector2 tilePosition) ; //Get the tile at the specified coordinate in the grid
+	void SetTile(int x, int y, Tile type); //St the tile at the specified coordinate in the grid
+	bool IsInsideGrid(int x, int y); //Returns true of the coordinate is insdie the grid, false otherwise
+	bool IsInsideGrid(Vector2 tilePosition); //Returns true of the coordinate is insdie the grid, false otherwise
+	Vector2 TilePosToScreenPos(Vector2 tilePosition); //Convert from a tile coordinate to a screen position
+	Vector2 TilePosToScreenPos(int x, int y); //Convert from a tile coordinate to a screen positon
+	Vector2 ScreenPosToTilePos(Vector2 postionOnScreen);//Find a tile coordinate given a position on the screen over a tile
+	
+	bool IsTraversible(Vector2 tilePosition);
+
+	void DrawBorders(Color color = BLACK);
+	void DrawTiles();
+	void RegnerateLevel(int chanceofWall = 50);
+
+	Vector2 GetTileCenter(Vector2 tilePosition);
+
+	void MoveSpriteUp( );
+	void MoveSpriteLeft();
+	void MoveSpriteDown();
+	void MoveSpriteRight();
+
+	void ReplacePlayer();
+
+	void DrawNodes();
+
+	void DrawConnections();
+
+	void DrawSprite();
+
+	std::vector<Vector2> GetTraversibleTilesAdjacentTo(Vector2 tilePosition);
+
+};
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,242 +1,271 @@
 #include "rlImGui.h"
 #include "Math.h"
+#include "TileMap.h"
 #include <iostream>
 #include <vector>
-#include "Agent.h"
-#include "GameObject.h"
-#include "Food.h"
-#include "Obstacle.h"
-#include "Predator.h"
-#include "Steeringbehaviors.h"
-#include <map>
-#include "TextureManager.h"
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+
+
+//        position = position + (m_fish->GetVelocity() * deltaTime) + ((acceleration * 0.5f) * deltaTime * deltaTime);
+//        position = WrapAroundScreen(position);
+//        m_fish->SetPosition(position);
+//        m_fish->SetVelocity(m_fish->GetVelocity() + acceleration * deltaTime);
+
+bool CheckCollisionLineCircle(Vector2 agentPosition,Vector2 obstaclePosition,Vector2 lineToCheckAgainst,float circleRadius)
+{
+    Vector2 NearestPointOnVector = NearestPoint(agentPosition, agentPosition + lineToCheckAgainst, obstaclePosition);
+
+    float distanceFromObstacleToPoint = Distance(NearestPointOnVector, obstaclePosition);
+    return  (distanceFromObstacleToPoint <= circleRadius);
+}
+
+Vector2 WrapAroundScreen(Vector2 position)
+{
+    Vector2 outPosition =
+    {
+        fmodf(position.x + SCREEN_WIDTH,SCREEN_WIDTH),
+        fmodf(position.y + SCREEN_HEIGHT,SCREEN_HEIGHT)
+    };
+    return outPosition;
+}
+float AngleFromVector(Vector2 direction)
+{
+    float angle = atan2f(direction.y, direction.x) * RAD2DEG;
+    angle = fmodf(angle + 360, 360.0f);
+    return angle;
+}
+
+Vector2 VectorFromAngleDegrees(float angleDegrees)
+{
+    return Vector2{ cosf(angleDegrees * DEG2RAD),sinf(angleDegrees * DEG2RAD) };
+}
+
+struct RigidBody
+{
+    Vector2 pos = {SCREEN_WIDTH/2,SCREEN_HEIGHT/2};
+    Vector2 accel = {};
+    Vector2 velo = {};
+    Vector2 dir = {};
+    float rotation;
+    float angularSpeed;
+};
+
+
+
+class Agent
+{
+public:
+
+
+    Agent(float r1,float r2,float l1,float l2,float maxAccel,float maxSpeed)
+    {
+        m_fish = new RigidBody();
+        m_maxAacceleration = maxAccel;
+        m_maxSpeed = maxSpeed;
+        whiskerLengthL1 = l1;
+        whiskerLengthL2 = l2;
+        whiskerLengthR1 = r1;
+        whiskerLengthR2 = r2;
+        m_fish->angularSpeed = 100;
+
+        whiskers = new Vector2[whiskerCount];
+        detection = new bool [whiskerCount];
+
+    }
+
+    ~Agent()
+    {
+        delete m_fish;
+        m_fish = nullptr;
+    }
+
+
+
+
+
+    void Avoid(Vector2 obstacle,float deltaTime,float radiusOfObstacle)
+    {
+        //direction = Rotate(direction, 50 * dt * DEG2RAD);
+
+        for (int i = 0; i < whiskerCount; i++)
+        {
+            detection[i] = CheckCollisionLineCircle(m_fish->pos, obstacle, whiskers[i], radiusOfObstacle);
+        }
+        
+        if (detection[1] || detection[0])
+        {
+            m_fish->velo = Rotate(m_fish->velo, m_fish->angularSpeed * deltaTime * DEG2RAD);
+        }
+
+        if (detection[2] || detection[3])
+        {
+            m_fish->velo = Rotate(m_fish->velo, -m_fish->angularSpeed * deltaTime * DEG2RAD);
+        }
+
+    }
+
+    void UpdateWhiskers()
+    {
+        whiskerAngleL1 = fmodf(m_fish->rotation - 15 + 360, 360.0f);
+        whiskerAngleL2 = fmodf(m_fish->rotation - 30 + 360, 360.0f);
+        whiskerAngleR1 = fmodf(m_fish->rotation + 15 + 360, 360.0f);
+        whiskerAngleR2 = fmodf(m_fish->rotation + 30 + 360, 360.0f);
+
+
+        whiskers[0] = VectorFromAngleDegrees(whiskerAngleL1) * whiskerLengthL1;
+        whiskers[1] = VectorFromAngleDegrees(whiskerAngleL2) * whiskerLengthL2;
+        whiskers[2] = VectorFromAngleDegrees(whiskerAngleR1) * whiskerLengthR1;
+        whiskers[3] = VectorFromAngleDegrees(whiskerAngleR2) * whiskerLengthR2;
+
+  
+
+
+    }
+
+    //Updates Movement
+    void Update(float deltaTime)
+    {
+         m_fish->rotation = AngleFromVector(Normalize(m_fish->velo));
+        UpdateWhiskers();
+
+
+
+        
+
+
+
+       float MagOfVelo = Length(m_fish->velo);
+       if (MagOfVelo > m_maxSpeed)
+       {
+           m_fish->velo = m_fish->velo * (m_maxSpeed / MagOfVelo);
+       };
+       m_fish->pos = m_fish->pos + (m_fish->velo * deltaTime) + ((m_fish->accel * 0.5f) * deltaTime * deltaTime);
+       m_fish->velo = m_fish->velo + m_fish->accel * deltaTime;
+       m_fish->pos = WrapAroundScreen(m_fish->pos);
+       m_fish->accel = {};
+
+      //  m_fish->dir = RotateTowards(m_fish->dir, Normalize(m_fish->velo), m_fish->angularSpeed * deltaTime);
+    }
+
+    
+    //Returns Acceleration Vector Towards obstacle
+    Vector2 Seek(Vector2 const  targetPosition, float deltaTIme)
+    {
+        Vector2 deltaAccel = Normalize(targetPosition - m_fish->pos) * m_maxSpeed - m_fish->velo;
+
+         m_fish->rotation = AngleFromVector(targetPosition-m_fish->pos);
+      //  m_fish->dir = VectorFromAngleDegrees(m_fish->rotation);
+        m_fish->accel = deltaAccel;
+        return deltaAccel;       
+    }
+
+
+    Vector2 Flee(float deltaTime, Vector2 targetPosition)
+    {
+        Vector2 deltaAccel = (Normalize(targetPosition - m_fish->pos) * m_maxSpeed - m_fish->velo)  -1;
+        return deltaAccel;
+    }
+
+    void Draw()
+    {
+        Vector2 veloNorm = Normalize(m_fish->velo);
+
+        DrawCircleV(m_fish->pos, circleRadius, BLACK);
+       // DrawLineV(m_fish->pos, m_fish->pos + veloNorm * 100, RED);
+        for (int i = 0; i < whiskerCount; i++)
+        {
+            DrawLineV(m_fish->pos, m_fish->pos + whiskers[i],(detection[i]) ? RED:GREEN);
+
+        }
+    }
+
+private:
+    float m_maxSpeed; // 350 Px/s 
+    float m_maxAacceleration; // 50 Px/s /s
+    float whiskerLengthL1;
+    float whiskerLengthL2;
+    float whiskerLengthR1;
+    float whiskerLengthR2;
+    RigidBody* m_fish;
+
+    float circleRadius = 40;
+
+
+    float whiskerAngleL1 ;
+    float whiskerAngleL2 ;
+    float whiskerAngleR1 ;
+    float whiskerAngleR2 ;
+
+    int whiskerCount = 4;
+
+    bool* detection;
+    Vector2* whiskers;
+
+    Vector2 whiskerLeft1 ;
+    Vector2 whiskerLeft2 ;
+    Vector2 whiskerRight1;
+    Vector2 whiskerRight2;
+};
+
+
+
+
+
 
 
 int main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "GAME3001 - Assignment 1");
-    Image icon = LoadImage("../game/assets/Fish/fish.png");
-    SetWindowIcon(icon);
+    
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     rlImGuiSetup(true);
     SetTargetFPS(60);
-    srand(time(NULL));
-
-    TextureManager::Load("../game/assets/Fish/fish.png", "fish");
-    TextureManager::Load("../game/assets/Fish/food.png", "food");
-    TextureManager::Load("../game/assets/Fish/enemy.png", "enemy");
-    TextureManager::Load("../game/assets/Fish/obstacle.png", "rock");
-    
-    //Creating bools for GameMode
-    std::map<std::string, bool> Mode;
-    Mode.emplace("Off", false);
-    Mode.emplace("Seek", false);
-    Mode.emplace("Arrive", false);
-    Mode.emplace("Flee", false);
-    Mode.emplace("Avoid", false);
-
-    std::vector<GameObject*> objects;
-    std::vector<Agent*> agents;
-
-    //Randomizing TopSpeed and Accleration Of fish
-    for (int i = 0; i < 15; i++)
-    {
-        Vector2 tempPos = {
-            rand() % SCREEN_WIDTH + 1,
-            rand() % SCREEN_HEIGHT + 1,
-        };
-
-        int randomMaxSpeed = 250 + (rand() % 151);
-        int randomMaxAccel = 250 + (rand() % 151);
-
-        if (i < 5) { randomMaxSpeed += 150; randomMaxAccel += 150; }
-        else if (i >=10) { randomMaxSpeed -= 150; randomMaxAccel -= 150; }
-
-
-
-        agents.push_back(new Agent(tempPos, 100, 100, randomMaxAccel, randomMaxSpeed,"fish"));
-    }
-
+    Tilemap map("../game/assets/p1_walk01.png");
+    map.player.SetSize(64, 64);
     float timer = 0;
 
-    const float radius = 30;
-    const float whiskerLength = 300;
+    int wallChance = 20;
+    map.RegnerateLevel(wallChance);
+    bool imGui = false;
 
-    Vector2 position = { SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2 };//in px
+    Vector2 position = { SCREEN_WIDTH/2,SCREEN_HEIGHT/2 };//in px
     Vector2 velocity = { 0,0 }; //In px/s
     float maxSpeed = 10;
     float maxAccel = 150;
     Vector2 acceleration = { 0,0 }; //In px/s/s
     Vector2 direction = { 13.0f,25.0f };
 
+
     Vector2 mousePOS = { 0,0 };
-        bool seek = false;
-        bool flee = false;
-        bool arrive = false;
 
     while (!WindowShouldClose())
     {
-        Vector2 tempAccel = { 0,0 };
         BeginDrawing();
-        ClearBackground(GRAY);
-        rlImGuiBegin();
+        ClearBackground(WHITE);
+            rlImGuiBegin();
+
+            if (IsKeyPressed(KEY_GRAVE)) imGui = !imGui;
+            if (imGui)
+            {
+                ImGui::SliderInt("Wall%", &wallChance, 0, 100, NULL);
+                if (ImGui::Button("Regenerate Level"))
+                {
+                    map.RegnerateLevel(wallChance);
+                }
+            }
+
+            if (IsKeyPressed(KEY_W)) map.MoveSpriteUp();
+            if (IsKeyPressed(KEY_S)) map.MoveSpriteDown();
+            if (IsKeyPressed(KEY_A)) map.MoveSpriteLeft();
+            if (IsKeyPressed(KEY_D)) map.MoveSpriteRight();
+            
 
         const float dt = GetFrameTime();
         mousePOS = GetMousePosition();
-
-        //  ImGui::SliderFloat2("position", &(position.x), 0, SCREEN_WIDTH);
-        //  ImGui::SliderFloat2("velocity", &(velocity.x), -maxSpeed, maxSpeed);
-        //  ImGui::SliderFloat2("Acceleration", &(acceleration.x), -maxAccel, maxAccel);
-        //  ImGui::SliderFloat("Max Acceleration", &maxAccel, 1, 1500); 
-        //  ImGui::SliderFloat("Max Speed", &maxSpeed, -1, 1500);
-
-        //****SET MODE****
-        if (IsKeyPressed(KEY_ONE))
-        {
-            for (auto& options : Mode)
-            {
-                options.second = false;
-            }
-            Mode["Seek"] = true;
-        }
-        if (IsKeyPressed(KEY_TWO))
-        {
-            for (auto& options : Mode)
-            {
-                options.second = false;
-            }
-            Mode["Flee"] = true;
-        }
-        if (IsKeyPressed(KEY_THREE))
-        {
-            for (auto& options : Mode)
-            {
-                options.second = false;
-            }
-            Mode["Arrive"] = true;
-        }
-        if (IsKeyPressed(KEY_FOUR))
-        {
-            for (auto& options : Mode)
-            {
-                options.second = false;
-            }
-            Mode["Avoid"] = true;
-        }
-        if (IsKeyPressed(KEY_ZERO))
-        {
-            for (auto& options : Mode)
-            {
-                options.second = false;
-            }
-        }
-
-        //****MODES****
-        if (Mode["Seek"])
-        {
-            DrawText("Seek Mode", 15, 20, 50, WHITE);
-            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-            {
-                for (Agent* fish : agents)
-                {
-                    Steeringbehaviors::Seek(fish, mousePOS);
-                }
-
-            }
-        }
-        else if (Mode["Arrive"])
-        {
-            DrawText("Arrive Mode", 15, 20, 50, WHITE);
-            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-            {
-                objects.push_back(new Food(mousePOS, 10,"food"));
-            }
-        }
-        else if (Mode["Flee"])
-        {
-
-            DrawText("Flee Mode", 15, 20, 50, WHITE);
-            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-            {
-                objects.push_back(new Predator(mousePOS,"enemy"));
-            }
-        }
-        else if (Mode["Avoid"])
-        {
-            DrawText("Avoid Mode", 15, 20, 50, WHITE);
-            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-            {
-                objects.push_back(new Obstacle(mousePOS,"rock"));
-            }
-        }
-
-        //****UPDATE AGENTS****
-        for (Agent* fish : agents)
-        {
-
-            for (GameObject* object : objects)
-            {
-                float distance = Distance(fish->GetPosition(), object->GetPosition());
-
-                if (distance < fish->Neighborhood.slowingRadius)
-                {
-                    if (object->GetType() == ObjectType::FoodType)
-                    {
-                    fish->Neighborhood.inHood = true;
-                        //Arrive to Food
-                        Steeringbehaviors::Arrive(fish, object);
-                        if (distance <= fish->Neighborhood.arrivedRadius)
-                        {
-                            object->Damage();
-                            if (object->IsAte())
-                            {
-                                object->RemoveFromScreen();
-                            }
-                        }
-                    }
-                     if (object->GetType() == ObjectType::ObstacleType)
-                        {
-                        fish->Neighborhood.inHood = true;
-                        //Avoid Obstacle
-                        Steeringbehaviors::Avoid(fish, object->GetPosition(), dt);
-                        }
-                     if (object->GetType() == ObjectType::Enemy)
-                     {
-                         fish->Neighborhood.inHood = true;
-                         Steeringbehaviors::Flee(fish, object->GetPosition());
-                     }
-                }
-                else fish->Neighborhood.inHood = false;
-            }
-
-        }
-
-        //****DRAW****
-        for (GameObject* object : objects)
-        {
-            object->Draw();
-        }
-
-        for (Agent* fish : agents)
-        {
-
-            fish->Draw();
-            fish->Update(dt);
-        }
-       
-        if (IsKeyPressed(KEY_SPACE))
-        {
-            for (GameObject* object : objects)
-            {
-                delete object;
-                object = nullptr;
-                objects.clear();
-            }
-        }
+        map.DrawTiles();
+        map.DrawBorders();
+        map.DrawNodes();
+        map.DrawSprite();
 
 
-        DrawCircleV(mousePOS, radius, RED);
 
         timer += dt;
         rlImGuiEnd();
@@ -244,6 +273,7 @@ int main(void)
     }
 
     CloseWindow();
+    //If you are curious about what numbers the hash function produces, try puttin this block of test code in main
     return 0;
 
 }
